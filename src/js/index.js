1cf05665a278d2b3280888a5860ee293c1eb291e
Frelay <makarovpavel@hotmail.com>
2023-06-06 15:15:46 +0500

    Add categories menu list layout and add listeners on them

--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,10 @@
 
 import "../css/style.css";
 import * as slider from "../scss/slider/slider.js";
+import * as categories from "../scss/categories/categories.js";
 
 window.addEventListener("DOMContentLoaded", () => {
     slider.autoShowSlides();
     slider.setActiveSlide();
+    categories.setActiveCategory();
 });
---
+++ src/scss/categories/categories.js
@@ -0,0 +1,22 @@
+"use strict";
+
+// Получаем элементы
+const categoryList = document.querySelector(".categories__list"),
+    categoryListItem = document.querySelectorAll(".categories__list-item");
+
+// Функция делает категорию активной
+export function setActiveCategory() {
+    categoryListItem[0].classList.add("active-category");
+
+    categoryList.addEventListener("click", (e) => {
+        if (e.target.classList.contains("categories__list-item")) {
+            categoryListItem.forEach((item) => {
+                if (item.classList.contains("active-category")) {
+                    item.classList.remove("active-category");
+                }
+            });
+
+            e.target.classList.add("active-category");
+        }
+    });
+}
